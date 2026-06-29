0b8de97ab947815ec9bf2cad2c36e80b4e22ee54
DarkGhostOfficial <darkghostofficial91@gmail.com>
2026-06-29 19:17:35 +0200

    Add Game.cpp/.hpp

---
+++ include/Game.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+    Game(); // It is a constructor that initializes the main variables.
+    void run(); // Initializes the game loop that repeats every frame.
+
+private:
+    void processEvent(); // handles events, input, etc...
+    void update(float dt); // It updates everything every frame.
+    void render(); // Renders everything to the screen.
+
+    sf::RenderWindow window;
+    sf::Clock clock;
+};
---
+++ src/Game.cpp
@@ -0,0 +1,39 @@
+#include "../include/Game.hpp"
+
+Game::Game() : window(sf::VideoMode({800, 600}), "APM")
+{
+    window.setFramerateLimit(60); // Sets the maximum fps to 60
+}
+
+void Game::run()
+{
+    while (window.isOpen()) // Initializes the game loop.
+    {
+        float dt = clock.restart().asSeconds(); // Initialize the delta time.
+
+        processEvent();
+        update(dt);
+        render();
+    }
+}
+
+void Game::processEvent()
+{
+    while (const std::optional event = window.pollEvent()) // Initializes the sequence of events, inputs, etc...
+    {
+        if (event->is<sf::Event::Closed>()) // If the window is closed, the loop terminates.
+        {
+            window.close();
+        }
+    }
+}
+
+void Game::update(float dt)
+{
+}
+
+void Game::render()
+{
+    window.clear(sf::Color::Black); // Sets the window color to black.
+    window.display(); // Show everything on screen
+}
